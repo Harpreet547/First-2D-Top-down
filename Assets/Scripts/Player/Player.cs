124e6db98096c296aabd047f20007ce84508b905
Harpreet547 <singhharpreet547@gmail.com>
2022-02-01 14:24:13 +0530

    Basic dash implementation done
    Simple dash logic added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,16 @@ public class Player : MonoBehaviour {
     Rigidbody2D body;
     Animator animator;
     Vector2 movement;
-    public float runSpeed = 5.0f;
+    public float runSpeed = 100.0f;
+
+    // Dashing variables
+    bool isDashing = false;
+    bool shouldCallDashCoroutine = false;
+    public float startDashTime = 0.05f;
+    public float dashSpeed = 25f;
+    // Number of times player can dash per second.
+    public float dashCooldown = 2f;
+    float nextDashTime = 0f;
 
     void Start() {
         body = GetComponent<Rigidbody2D>();
@@ -20,18 +29,40 @@ public class Player : MonoBehaviour {
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(Time.time >= nextDashTime) {
+            if(Input.GetKeyDown(KeyCode.X)) {
+                nextDashTime = Time.time + dashCooldown;
+                shouldCallDashCoroutine = true;
+            }
+        }
     }
 
     private void FixedUpdate() {
-        if(!IsPlaying(animator, "Sword_attack")) {
-            body.MovePosition(body.position + (Time.fixedDeltaTime * runSpeed * movement));
+        if(!IsPlaying(animator, "Sword_attack") && !isDashing) {
+            // body.MovePosition(body.position + (Time.fixedDeltaTime * runSpeed * movement));
+            body.velocity = Time.fixedDeltaTime * runSpeed * (new Vector2(movement.x, movement.y));
+        }
+
+        if(shouldCallDashCoroutine) {
+            StartCoroutine(DashForward());
+            shouldCallDashCoroutine = false;
         }
     }
 
     bool IsPlaying(Animator anim, string stateName) {
-        Debug.Log(anim.GetCurrentAnimatorStateInfo(0).IsName(stateName));
 
         return anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
             anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f;
+    }
+
+    IEnumerator DashForward() {
+        isDashing = true;
+        body.velocity *=  dashSpeed;
+        // body.AddForce(new Vector2(body.velocity.x, body.velocity.y)  * dashSpeed, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(startDashTime);
+        isDashing = false;
+        body.velocity = new Vector2(0, 0);
+
     }
 }
